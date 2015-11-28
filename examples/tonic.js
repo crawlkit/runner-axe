e998af77fbfac8979cd9173ebe685150e937c505
Joscha Feth <joscha@feth.com>
2015-11-29 01:55:43 +1100

    docs: add tonic example

---
+++ examples/tonic.js
@@ -0,0 +1,10 @@
+const CrawlKit = require('crawlkit');
+const AxeRunner = require('crawlkit-runner-axe');
+
+const crawler = new CrawlKit('http://www.google.com');
+crawler.addRunner('aXe', new AxeRunner());
+
+crawler.crawl()
+    .then((data) => {
+        console.log(JSON.stringify(data.results, true, 2));
+    });
